da758044b4ee7ee02e2d0f63918805d8946bfaf0
LS--20160803ISH\Administrator <897622361@qq.com>
2017-03-28 11:03:59 +0800

    test1

---
+++ Assert/less5.js
@@ -0,0 +1,15 @@
+/*
+* @Author: Administrator
+* @Date:   2017-03-22 17:01:26
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-03-22 17:02:45
+*/
+
+'use strict';
+const assert = require('assert')
+
+ assert.ifError(0)
+
+ // assert.ifError(1)
+
+assert.ifError('error')
